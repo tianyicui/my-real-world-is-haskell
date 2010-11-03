33e07d6b1fa9c1854a682ec31ae13250699bfa70
Tianyi Cui <tianyicui@gmail.com>
2010-11-03 17:01:08 +0800

    ex p60.2

--- ch03/TreeADT.hs
+++ ch03/TreeADT.hs
@@ -1,3 +1,9 @@
+module TreeADT where
+
 data Tree a = Node a (Tree a) (Tree a)
             | Empty
               deriving (Show)
+
+-- Ex p60.2
+data MaybeTree a = JustNode a (Maybe (MaybeTree a)) (Maybe (MaybeTree a))
+                   deriving (Show)
