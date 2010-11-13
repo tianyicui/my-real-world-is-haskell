64384643a19649e2e024a3dcde800376343c1c98
Tianyi Cui <tianyicui@gmail.com>
2010-11-13 19:19:08 +0800

    ch03: wip

--- ch03/Guard.hs
+++ ch03/Guard.hs
@@ -10,3 +10,11 @@ fromMaybe defval wrapped =
     case wrapped of
         Nothing    -> defval
         Just value -> value
+
+-- Note: the variable name 'otherwise' is not anything special
+lend amount balance
+    | amount <= 0             = Nothing
+    | amount >  reserve * 0.5 = Nothing
+    | otherwise               = Just newBalance
+   where reserve    = 100
+         newBalance = balance - amount
