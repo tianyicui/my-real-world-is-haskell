c2289b32608406f45f74ddf42a8384b34cf66744
Tianyi Cui <tianyicui@gmail.com>
2010-11-13 19:11:07 +0800

    ch03: BadTree

---
+++ ch03/BadTree.hs
@@ -0,0 +1,11 @@
+module BadTree where
+
+import TreeADT
+
+-- This won't compile
+-- bad_nodesAreSame (Node a _ _) (Node a _ _) = Just a
+-- bad_nodesAreSame _            _            = Nothing
+
+nodesAreSame (Node a _ _) (Node b _ _)
+    | a == b     = Just a
+nodesAreSame _ _ = Nothing
