54e5e21b35d902d04b3b534b57f9d7f81172e13e
Tianyi Cui <tianyicui@gmail.com>
2010-11-16 19:26:38 +0800

    ex3.9

--- ch03/TreeADT.hs
+++ ch03/TreeADT.hs
@@ -7,3 +7,7 @@ data Tree a = Node a (Tree a) (Tree a)
 -- Ex p60.2
 data MaybeTree a = JustNode a (Maybe (MaybeTree a)) (Maybe (MaybeTree a))
                    deriving (Show)
+
+-- Ex 3.9
+height Empty = 0
+height (Node _ x y) = 1 + (max (height x) (height y))
