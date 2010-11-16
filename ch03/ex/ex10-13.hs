d1039f330714fc349f4e63b960adc1aa97296030
Tianyi Cui <tianyicui@gmail.com>
2010-11-16 19:48:01 +0800

    ex3.11

--- ch03/ex/ex10-13.hs
+++ ch03/ex/ex10-13.hs
@@ -3,3 +3,12 @@ module Graham where
 data Direction = TurnLeft
                | TurnRight
                | StraightLine
+                 deriving Show
+
+data Point a = Point a a
+
+turn (Point x1 y1) (Point x2 y2) (Point x3 y3) =
+    case signum ((x2-x1)*(y3-y1) - (y2-y1)*(x3-x1)) of
+        1  -> TurnLeft
+        -1 -> TurnRight
+        0  -> StraightLine
