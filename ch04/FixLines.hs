2e315914480b459b42566a7c4315b3ec2103abbc
Tianyi Cui <tianyicui@gmail.com>
2010-11-20 23:22:15 +0800

    refactored FixLines

--- ch04/FixLines.hs
+++ ch04/FixLines.hs
@@ -2,17 +2,6 @@ import System.Environment (getArgs)
 
 import SplitLines (fixLines)
 
-interactWith function inputFile outputFile = do
-  input <- readFile inputFile
-  writeFile outputFile (function input)
+import InteractWith (mainWith)
 
-main = mainWith myFunction
-  where mainWith function = do
-          args <- getArgs
-          case args of
-            [input,output] -> interactWith function input output
-            _ -> putStrLn "error: exactly two arguments needed"
-
-        -- the function id can be replaced by anything
-        -- that have the type String -> String
-        myFunction = fixLines
+main = mainWith fixLines
