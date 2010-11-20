e979f97428959bb48fca60ce4d2c4623efea3131
Tianyi Cui <tianyicui@gmail.com>
2010-11-20 22:59:31 +0800

    ch04: InteractWith

---
+++ ch04/InteractWith.hs
@@ -0,0 +1,16 @@
+import System.Environment (getArgs)
+
+interactWith function inputFile outputFile = do
+  input <- readFile inputFile
+  writeFile outputFile (function input)
+
+main = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+            [input,output] -> interactWith function input output
+            _ -> putStrLn "error: exactly two arguments needed"
+
+        -- the function id can be replaced by anything
+        -- that have the type String -> String
+        myFunction = id
