07455b3021217ad63637579bc8dd5d84ced96e6d
Tianyi Cui <tianyicui@gmail.com>
2010-11-21 15:15:10 +0800

    ch04: fold wip

--- ch04/Fold.hs
+++ ch04/Fold.hs
@@ -1,4 +1,4 @@
-module Fold where
+import Prelude(id, flip, ($))
 
 -- implementation of foldl and foldr
 
@@ -11,13 +11,20 @@ foldr f = go where
     go z []     = z
 
 -- implement filter using foldr
-filter p = Fold.foldr step [] where
+filter p = foldr step [] where
     step x xs = if p x then x : xs else xs
 
 -- map using foldr
-map f = Fold.foldr step [] where
+map f = foldr step [] where
     step x xs = f x : xs
 
 -- foldl using foldr
-foldl' f z xs = Fold.foldr step id xs z where
+foldl_using_foldr f z xs =
+    foldr step id xs z where
     step x g a = g (f a x)
+
+identity :: [a] -> [a]
+identity = foldr (:) []
+
+append :: [a] -> [a] -> [a]
+append = flip $ foldr (:)
