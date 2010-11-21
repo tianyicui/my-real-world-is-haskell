631c6a60ec538f7e58f8ce754f1edd31d675578b
Tianyi Cui <tianyicui@gmail.com>
2010-11-21 10:41:07 +0800

    my transpose implementation

--- ch04/ex/p84/ex3.hs
+++ ch04/ex/p84/ex3.hs
@@ -1,4 +1,9 @@
 import InteractWith (mainWith)
-import Data.List (transpose)
 
 main = mainWith $ unlines . transpose . lines
+
+transpose [] = []
+transpose [xs] = map (\x -> [x]) xs
+transpose (x:xs) =
+    let ys = transpose xs
+    in zipWith (:) x ys
