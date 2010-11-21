d951c94ce789621b68f55ebdf087f27a5809050e
Tianyi Cui <tianyicui@gmail.com>
2010-11-21 15:46:25 +0800

    ex3

--- ch04/ex/p98/ex1-3.hs
+++ ch04/ex/p98/ex1-3.hs
@@ -1,6 +1,14 @@
-import Data.Char (digitToInt)
+import Data.Char (digitToInt, isNumber)
+
+asIntError = error "asInt: parse error"
+
+asInt "" = asIntError
 
 asInt ('-':xs) = - (asInt xs)
 
 asInt xs = foldl step 0 xs where
-    step a b = 10*a + digitToInt b
+    step a b
+        | isNumber b = 10*a + digitToInt b
+        | otherwise = asIntError
+
+-- TODO: overflow handling
