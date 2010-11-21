687d05ef9b6076beef999296472cdbcd620fc2e6
Tianyi Cui <tianyicui@gmail.com>
2010-11-21 11:15:39 +0800

    The standard version of transpose

--- ch04/ex/p84/ex3.hs
+++ ch04/ex/p84/ex3.hs
@@ -2,8 +2,14 @@ import InteractWith (mainWith)
 
 main = mainWith $ unlines . transpose . lines
 
-transpose [] = []
-transpose [xs] = map (\x -> [x]) xs
-transpose (x:xs) =
-    let ys = transpose xs
-    in zipWith (:) x ys
+--- stolen from http://haskell.org/onlinereport/list.html
+
+-- transpose is lazy in both rows and columns,
+--       and works for non-rectangular 'matrices'
+-- For example, transpose [[1,2],[3,4,5],[]]  =  [[1,3],[2,4],[5]]
+-- Note that [h | (h:t) <- xss] is not the same as (map head xss)
+--      because the former discards empty sublists inside xss
+transpose []             = []
+transpose ([]     : xss) = transpose xss
+transpose ((x:xs) : xss) = (x : [ h | (h:t) <- xss]) :
+                           transpose (xs : [t | (h:t) <- xss])
