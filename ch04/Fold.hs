5621fd4f28adb59b46a112a3860ad8f3338d06f7
Tianyi Cui <tianyicui@gmail.com>
2010-11-21 15:02:50 +0800

    ch04: fold

--- ch04/Fold.hs
+++ ch04/Fold.hs
@@ -1,3 +1,7 @@
+module Fold where
+
+-- implementation of foldl and foldr
+
 foldl f = go where
     go z (x:xs) = go (f z x) xs
     go z []     = z
@@ -5,3 +9,15 @@ foldl f = go where
 foldr f = go where
     go z (x:xs) = f x $ go z xs
     go z []     = z
+
+-- implement filter using foldr
+filter p = Fold.foldr step [] where
+    step x xs = if p x then x : xs else xs
+
+-- map using foldr
+map f = Fold.foldr step [] where
+    step x xs = f x : xs
+
+-- foldl using foldr
+foldl' f z xs = Fold.foldr step id xs z where
+    step x g a = g (f a x)
