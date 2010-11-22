e6261c100aa2df8bd6f55e127cc9be1a929cfcbc
Tianyi Cui <tianyicui@gmail.com>
2010-11-22 23:01:34 +0800

    ch04: composition

--- ch04/SuffixTree.hs
+++ ch04/SuffixTree.hs
@@ -1,2 +1,14 @@
+import Data.List (tails)
+
 suffixes xs@(_:xs') = xs : suffixes xs'
 suffixes _ = []
+
+suffixes2 xs = init (tails xs)
+
+compose f g x = f (g x)
+
+suffixes3 xs = compose init tails xs
+
+suffixes4 = compose init tails
+
+suffixes5 = init . tails
