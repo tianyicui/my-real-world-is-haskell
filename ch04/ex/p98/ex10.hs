37cbccf8bfcfc1b7d6f6a9f0f5f2e400504bdf76
Tianyi Cui <tianyicui@gmail.com>
2010-11-22 22:03:34 +0800

    ex p98.10.3

--- ch04/ex/p98/ex10.hs
+++ ch04/ex/p98/ex10.hs
@@ -1,3 +1,13 @@
 any_foldr f = foldr (\x y -> f x || y) False
 
 cycle_foldr xs = foldr (:) (cycle_foldr xs) xs
+
+-- XXX: this looks ugly!
+words_foldr xs = tail $ foldr step [] (' ':xs) where
+    step ' '  xs = emptyHead xs
+    step '\n' xs = emptyHead xs
+    step '\r' xs = emptyHead xs
+    step ch (x:xs) = (ch : x) : xs
+    step ch [] = [[ch]]
+    emptyHead l@("":xs) = l
+    emptyHead xs = "" : xs
