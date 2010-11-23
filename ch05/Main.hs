f4495145b00d4053486d7d6998f5b280f5dd761d
Tianyi Cui <tianyicui@gmail.com>
2010-11-23 13:26:21 +0800

    ch05: PutJSON.hs

--- ch05/Main.hs
+++ ch05/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
 import SimpleJSON
+import PutJSON (putJValue)
 
-main = print (JObject [("foo", JNumber 1), ("bar", JBool False)])
+main = putJValue (JObject [("foo", JNumber 1), ("bar", JBool False)])
---
+++ ch05/PutJSON.hs
@@ -0,0 +1,25 @@
+module PutJSON where
+
+import Data.List (intercalate)
+import SimpleJSON
+
+renderJValue :: JValue -> String
+
+renderJValue (JString s)   = show s
+renderJValue (JNumber n)   = show n
+renderJValue (JBool True)  = "true"
+renderJValue (JBool False) = "false"
+renderJValue JNull         = "null"
+
+renderJValue (JObject o) = "{" ++ pairs o ++ "}"
+  where pairs [] = ""
+        pairs ps = intercalate ", " (map renderPair ps)
+        renderPair (k,v) = show k ++ ": " ++ renderJValue v
+
+renderJValue (JArray a) = "[" ++ values a ++ "]"
+  where values [] = ""
+        values vs = intercalate ", " (map renderJValue vs)
+
+-- NOTE: we separate the rendering code from the code that actually prints a value
+putJValue :: JValue -> IO ()
+putJValue v = putStrLn (renderJValue v)
