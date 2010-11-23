5260c2094d04202e26c805c50ec62a7bf143f47d
Tianyi Cui <tianyicui@gmail.com>
2010-11-23 19:53:31 +0800

    ch06: color.hs

---
+++ ch06/color.hs
@@ -0,0 +1,16 @@
+data Color = Red | Green | Blue
+
+instance Show Color where
+    show Red   = "Red"
+    show Green = "Green"
+    show Blue  = "Blue"
+
+instance Read Color where
+    readsPrec _ value =
+        tryParse [("Red", Red), ("Green", Green), ("Blue", Blue)]
+        where
+        tryParse [] = []
+        tryParse ((attempt, result):xs) =
+                if (take (length attempt) value) == attempt
+                   then [(result, drop (length attempt) value)]
+                   else tryParse xs
