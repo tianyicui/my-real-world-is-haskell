bfd3e0f90bc3fabb124bc0981b0def83ccaabc94
Tianyi Cui <tianyicui@gmail.com>
2010-11-23 19:36:40 +0800

    ch06: eqclasses 2 & 3

--- ch06/eqclasses.hs
+++ ch06/eqclasses.hs
@@ -5,3 +5,16 @@ instance BasicEq Bool where
     isEqual True  True  = True
     isEqual False False = True
     isEqual _     _     = False
+
+-- an instance of this class will be required to define two functions
+class BasicEq2 a where
+    isEqual2    :: a -> a -> Bool
+    isNotEqual2 :: a -> a -> Bool
+
+-- an instance of this class will be required to define only one function
+class BasicEq3 a where
+    isEqual3 :: a -> a -> Bool
+    isEqual3 x y = not (isNotEqual3 x y)
+
+    isNotEqual3 :: a -> a -> Bool
+    isNotEqual3 x y = not (isEqual3 x y)
