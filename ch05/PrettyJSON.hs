a9284ae0fe3bb55ae9b17389ac39fc4d776ec109
Tianyi Cui <tianyicui@gmail.com>
2010-11-23 13:44:21 +0800

    ch05: wip

--- ch05/PrettyJSON.hs
+++ ch05/PrettyJSON.hs
@@ -1,6 +1,7 @@
 module PrettyJSON where
 
 import SimpleJSON
+import PrettyStub
 
 renderJValue :: JValue -> Doc
 renderJValue (JBool True)  = text "true"
@@ -14,3 +15,17 @@ string = enclose '"' '"' . hcat . map oneChar
 -- NOTE: this is written in point-free style
 -- the "Pointy" equivalence is
 -- string s = enclose '"' '"' (hcat (map oneChar s))
+
+enclose :: Char -> Char -> Doc -> Doc
+enclose left right x = char left <> x <> char right
+
+oneChar :: Char -> Doc
+oneChar c = case lookup c simpleEscapes of
+              Just r -> text r
+              Nothing | mustEscape c -> hexEscape c
+                      | otherwise    -> char c
+    where mustEscape c = c < ' ' || c == '\x7f' || c > '\xff'
+
+simpleEscapes :: [(Char, String)]
+simpleEscapes = zipWith ch "\b\n\f\r\t\\\"/" "bnfrt\\\"/"
+    where ch a b = (a, ['\\',b])
