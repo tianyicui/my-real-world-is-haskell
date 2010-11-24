4bf0decfc588471697830ac1164e208e8fc246e1
Tianyi Cui <tianyicui@gmail.com>
2010-11-24 12:12:42 +0800

    ch07: better InteractWith

--- ch04/InteractWith.hs
+++ ch04/InteractWith.hs
@@ -10,4 +10,6 @@ mainWith function = do
   args <- getArgs
   case args of
     [input,output] -> interactWith function input output
-    _ -> putStrLn "error: exactly two arguments needed"
+    [input]        -> interactWith function input "/dev/stdout"
+    []             -> interactWith function "/dev/stdin" "/dev/stdout"
+    _ -> putStrLn "error: zero to two arguments needed"
---
+++ ch07/InteractWith.hs
@@ -0,0 +1,1 @@
+../ch04/InteractWith.hs
