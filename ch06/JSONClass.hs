102ef519dfa8400180c379b82c927153a5f26dec
Tianyi Cui <tianyicui@gmail.com>
2010-11-24 10:59:13 +0800

    ch06: JSONClass wip

--- ch06/JSONClass.hs
+++ ch06/JSONClass.hs
@@ -2,7 +2,13 @@
 
 module JSONClass where
 
-import SimpleJSON
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject (JObj JValue)
+            | JArray (JAry JValue)
+              deriving (Eq, Ord, Show)
 
 type JSONError = String
 
@@ -46,3 +52,37 @@ newtype JAry a = JAry {
 
 jary :: [a] -> JAry a
 jary = JAry
+
+newtype JObj a = JObj {
+      fromJObj :: [(String, a)]
+    } deriving (Eq, Ord, Show)
+
+jaryFromJValue :: (JSON a) => JValue -> Either JSONError (JAry a)
+jaryFromJValue (JArray (JAry a)) =
+    whenRight JAry (mapEithers fromJValue a) where
+        whenRight :: (b -> c) -> Either a b -> Either a c
+        whenRight _ (Left err) = Left err
+        whenRight f (Right a) = Right (f a)
+        mapEithers :: (a -> Either b c) -> [a] -> Either b [c]
+        mapEithers f (x:xs) = case mapEithers f xs of
+            Left err -> Left err
+            Right ys -> case f x of
+                Left err -> Left err
+                Right y -> Right (y:ys)
+        mapEithers _ _ = Right []
+
+jaryToJValue :: (JSON a) => JAry a -> JValue
+jaryToJValue = JArray . JAry . map toJValue . fromJAry
+
+instance (JSON a) => JSON (JAry a) where
+    toJValue = jaryToJValue
+    fromJValue = jaryFromJValue
+
+listToJValues :: (JSON a) => [a] -> [JValue]
+listToJValues = map toJValue
+
+jvaluesToJAry :: [JValue] -> JAry JValue
+jvaluesToJAry = JAry
+
+jaryOfJValuesToJValue :: JAry JValue -> JValue
+jaryOfJValuesToJValue = JArray
