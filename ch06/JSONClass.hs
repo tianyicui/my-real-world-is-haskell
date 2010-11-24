e760bb01697efef9d8ef5eba8e87416f61725538
Tianyi Cui <tianyicui@gmail.com>
2010-11-24 09:12:44 +0800

    ch06: JSONClass 3

--- ch06/JSONClass.hs
+++ ch06/JSONClass.hs
@@ -23,3 +23,19 @@ instance JSON String where
     toJValue = JString
     fromJValue (JString s) = Right s
     fromJValue _ = Left "not a JSON string"
+
+doubleToJValue :: (Double -> a) -> JValue -> Either JSONError a
+doubleToJValue f (JNumber v) = Right (f v)
+doubleToJValue _ _ = Left "not a JSON number"
+
+instance JSON Int where
+    toJValue = JNumber . realToFrac
+    fromJValue = doubleToJValue round
+
+instance JSON Integer where
+    toJValue = JNumber . realToFrac
+    fromJValue = doubleToJValue round
+
+instance JSON Double where
+    toJValue = JNumber
+    fromJValue = doubleToJValue id
