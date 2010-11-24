48f0e60e310578af9122c6f00e8ee04678ca9ea7
Tianyi Cui <tianyicui@gmail.com>
2010-11-24 09:08:44 +0800

    ch06: JSONClass 2

--- ch06/JSONClass.hs
+++ ch06/JSONClass.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+
 module JSONClass where
 
 import SimpleJSON
@@ -11,3 +13,13 @@ class JSON a where
 instance JSON JValue where
     toJValue = id
     fromJValue = Right
+
+instance JSON Bool where
+    toJValue = JBool
+    fromJValue (JBool b) = Right b
+    fromJValue _ = Left "not a JSON boolean"
+
+instance JSON String where
+    toJValue = JString
+    fromJValue (JString s) = Right s
+    fromJValue _ = Left "not a JSON string"
