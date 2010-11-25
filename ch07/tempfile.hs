0d44b08c98d9c862e06a9d5402e7cc224d9ce230
Tianyi Cui <tianyicui@gmail.com>
2010-11-26 02:36:40 +0800

    ch07: tempfile

--- ch07/tempfile.hs
+++ ch07/tempfile.hs
@@ -7,3 +7,39 @@ import Control.Exception(finally)
 
 main :: IO ()
 main = withTempFile "mytemp.txt" myAction
+
+myAction :: FilePath -> Handle -> IO ()
+myAction tempname temph =
+    do putStrLn "Welcome"
+       putStrLn $ "Temporary file at " ++ tempname
+
+       pos <- hTell temph
+       putStrLn $ "Initial pos: " ++ show pos
+
+       let tempdata = show [1..10]
+       putStrLn $ "Writing one line with " ++
+                  show (length tempdata) ++ " bytes: " ++
+                  tempdata
+       hPutStrLn temph tempdata
+
+       pos <- hTell temph
+       putStrLn $ "After writing, pos: " ++ show pos
+
+       putStrLn $ "The file content is: "
+       hSeek temph AbsoluteSeek 0
+
+       c <- hGetContents temph
+
+       putStrLn c
+
+       putStrLn $ "Which could be expressed as this Haskell literal:"
+       print c
+
+withTempFile :: String -> (FilePath -> Handle -> IO a) -> IO a
+withTempFile pattern func = do
+    tempdir <- catch (getTemporaryDirectory) (\_ -> return ".")
+    (tempfile, temph) <- openTempFile tempdir pattern
+
+    finally (func tempfile temph)
+            (do hClose temph
+                removeFile tempfile)
