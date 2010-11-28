19bf85bb2f2166c52d6f06fb96f80afba0e079cf
Tianyi Cui <tianyicui@gmail.com>
2010-11-29 03:34:03 +0800

    ch09: ControlledVisit.hs

--- ch09/ControlledVisit.hs
+++ ch09/ControlledVisit.hs
@@ -78,3 +78,20 @@ lesserP' = liftP' (<)
 traverseP, traverseP' :: (Info -> Bool) -> ([Info] -> [Info]) -> FilePath -> IO [Info]
 traverseP filt order path = return.(filter filt) =<< (traverse order path)
 traverseP' filt order = traverse $ order . filter filt
+
+-------------
+
+traverseVerbose order path = do
+    names <- getDirectoryContents path
+    let usefulNames = filter (`notElem` [".", ".."]) names
+    contents <- mapM getEntryName ("" : usefulNames)
+    recursiveContents <- mapM recurse (order contents)
+    return (concat recursiveContents)
+  where getEntryName name = getInfo (path </> name)
+        isDirectory info = case infoPerms info of
+                             Nothing -> False
+                             Just perms -> searchable perms
+        recurse info = do
+            if isDirectory info && infoPath info /= path
+                then traverseVerbose order (infoPath info)
+                else return [info]
