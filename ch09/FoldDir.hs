f3e764efefff81aba52567d463987325860dff89
Tianyi Cui <tianyicui@gmail.com>
2010-11-29 03:43:49 +0800

    FoldDir.hs: foldTree

--- ch09/FoldDir.hs
+++ ch09/FoldDir.hs
@@ -1,6 +1,7 @@
 module FoldDir where
 
 import ControlledVisit
+import System.FilePath ((</>))
 
 data Iterate seed = Done     { unwrap :: seed }
                   | Skip     { unwrap :: seed }
@@ -8,3 +9,25 @@ data Iterate seed = Done     { unwrap :: seed }
                     deriving (Show)
 
 type Iterator seed = seed -> Info -> Iterate seed
+
+foldTree :: Iterator a -> a -> FilePath -> IO a
+foldTree iter initSeed path = do
+    endSeed <- fold initSeed path
+    return (unwrap endSeed)
+  where
+    fold seed subpath = getUsefulContents subpath >>= walk seed
+
+    walk seed (name:names) = do
+      let path' = path </> name
+      info <- getInfo path'
+      case iter seed info of
+        done@(Done _) -> return done
+        Skip seed'    -> walk seed' names
+        Continue seed'
+          | isDirectory info -> do
+              next <- fold seed' path'
+              case next of
+                done@(Done _) -> return done
+                seed''        -> walk (unwrap seed'') names
+          | otherwise -> walk seed' names
+    walk seed _ = return (Continue seed)
