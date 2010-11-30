e3a0acd31409b79b94fa72d415716f28f6f5fddd
Tianyi Cui <tianyicui@gmail.com>
2010-11-30 16:11:28 +0800

    PNM: 2

--- ch10/PNM.hs
+++ ch10/PNM.hs
@@ -2,9 +2,14 @@ import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Lazy as L
 import Data.Char (isSpace)
 
-data Graymap = Graymap {
+data Greymap = Greymap {
       greyWidth :: Int
     , greyHeight :: Int
     , greyMax :: Int
     , greyData :: L.ByteString
     }
+
+instance Show Greymap where
+    show (Greymap w h m _) =
+        "Greymap " ++ show w ++ "x" ++ show h ++
+        " " ++ show m
