4305236df9b82ba3faa39f36fc2bcc8bd475f545
Tianyi Cui <tianyicui@gmail.com>
2011-01-23 08:00:09 +0800

    Parse.hs wip

---
+++ ch10/Parse.hs
@@ -0,0 +1,27 @@
+import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Lazy as L
+import Data.Int
+
+data ParseState = ParseState {
+      string :: L.ByteString
+    , offset :: Int64
+    } deriving (Show)
+
+simpleParse :: ParseState -> (a, ParseState)
+simpleParse = undefined
+
+betterParse :: ParseState -> Either String (a, ParseState)
+betterParse = undefined
+
+newtype Parse a = Parse {
+      runParse :: ParseState -> Either String (a, ParseState)
+    }
+
+identity :: a -> Parse a
+identity a = Parse (\s -> Right (a, s))
+
+parse :: Parse a -> L.ByteString -> Either String a
+parse parser initState
+    = case runParse parser (ParseState initState 0) of
+        Left err          -> Left err
+        Right (result, _) -> Right result
