9d9b880be103a52eeb1367597a663156ee65f126
Tianyi Cui <tianyicui@gmail.com>
2011-01-24 10:45:22 +0800

    ch10: Parse.hs

--- ch10/Parse.hs
+++ ch10/Parse.hs
@@ -1,6 +1,7 @@
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Lazy as L
-import Data.Int
+import Data.Int (Int64)
+import Data.Word (Word8)
 
 data ParseState = ParseState {
       string :: L.ByteString
@@ -25,3 +26,39 @@ parse parser initState
     = case runParse parser (ParseState initState 0) of
         Left err          -> Left err
         Right (result, _) -> Right result
+
+modifyOffset :: ParseState -> Int64 -> ParseState
+modifyOffset initState newOffset =
+    initState { offset = newOffset }
+
+parseByte :: Parse Word8
+parseByte =
+    getState ==> \initState ->
+    case L.uncons (string initState) of
+      Nothing ->
+          bail "no more input"
+      Just (byte, remainder) ->
+          putState newState ==> \_ ->
+          identity byte
+        where newState = initState { string = remainder,
+                                     offset = newOffset }
+              newOffset = offset initState + 1
+
+getState :: Parse ParseState
+getState = Parse (\s -> Right (s, s))
+
+putState :: ParseState -> Parse ()
+putState s = Parse (\_ -> Right ((), s))
+
+bail :: String -> Parse a
+bail err = Parse $ \s -> Left $
+           "byte offset " ++ show (offset s) ++ ": " ++ err
+
+(==>) :: Parse a -> (a -> Parse b) -> Parse b
+firstParser ==> secondParser = Parse chainedParser
+  where chainedParser initState =
+          case runParse firstParser initState of
+            Left errMessage ->
+                Left errMessage
+            Right (firstResult, newState) ->
+                runParse (secondParser firstResult) newState
