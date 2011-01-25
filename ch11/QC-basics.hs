ad12810226bed36c48e10f37b70dd14a578825bc
Tianyi Cui <tianyicui@gmail.com>
2011-01-25 11:12:53 +0800

    ch11/QC-basics

---
+++ ch11/QC-basics.hs
@@ -0,0 +1,8 @@
+import Test.QuickCheck
+import Data.List
+
+qsort :: Ord  => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
+    where lhs = filter (< x) xs
+          rhs = filter (>= x) xs
